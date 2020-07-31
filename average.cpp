90eebb3741a808a7f8c2652b7223a0ee1b54e1c8
Anjali-sinha <54208011+Anjali-sinha@users.noreply.github.com>
2020-07-31 21:14:57 +0530

    Add files via upload

---
+++ average.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float num1,num2,sum,average;
+    cout << "Enter two numbers:";
+    cin >> num1;
+    cin >> num2;
+    sum=num1+num2;
+    average=sum/2;
+    cout << "Sum="<<Sum;
+    cout << "Average=" <<average;
+    return 0;
+}
